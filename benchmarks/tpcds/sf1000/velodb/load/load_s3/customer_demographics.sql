7112f009079d780784e67ee97f051128ea2cd148
zgxme <zhenggaoxiong@selectdb.com>
2026-09-04 18:21:10 +0800

    fix tpcds velodb s3 load credentials (#45)

--- benchmarks/tpcds/sf1000/velodb/load/load_s3/customer_demographics.sql
+++ benchmarks/tpcds/sf1000/velodb/load/load_s3/customer_demographics.sql
@@ -8,6 +8,8 @@ LOAD LABEL customer_demographics_${TIMESTAMP}
 )
 WITH S3
 (
+    "AWS_ACCESS_KEY" = "${STORAGE_ACCESS_KEY}",
+    "AWS_SECRET_KEY" = "${STORAGE_SECRET_KEY}",
     "AWS_ENDPOINT" = "${STORAGE_ENDPOINT}",
     "AWS_REGION" = "${STORAGE_REGION}",
     "use_path_style" = "false"
